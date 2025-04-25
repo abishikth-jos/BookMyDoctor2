e76078d0bbd4b5c4532256c35f06c182bf1daab1
Aʙɪsʜɪᴋᴛʜ Jᴏs <73195851+abishikth-jos@users.noreply.github.com>
2025-04-25 11:37:37 +0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Doctors from './pages/Doctors'
-import Login from './pages/Login'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Appointment from './pages/Appointment'
-import MyAppointments from './pages/MyAppointments'
-import MyProfile from './pages/MyProfile'
-import Footer from './components/Footer'
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Doctors from './pages/Doctors';
+import Login from './pages/Login';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Appointment from './pages/Appointment';
+import MyAppointments from './pages/MyAppointments';
+import MyProfile from './pages/MyProfile';
+import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import socket from './socket'; // Import socket connection from socket.js
 
 const App = () => {
+  useEffect(() => {
+    // Listen for events or do something when socket is connected
+    socket.on('some-event', (data) => {
+      console.log('Received from server:', data);
+    });
+
+    // Cleanup: disconnect socket when component is unmounted
+    return () => {
+      socket.off('some-event'); // Remove event listeners
+    };
+  }, []); // Empty array ensures this effect runs only once, like componentDidMount
+
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <ToastContainer />
@@ -31,7 +44,7 @@ const App = () => {
       </Routes>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
