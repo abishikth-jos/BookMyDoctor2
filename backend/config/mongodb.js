bd0a23a2a60e5166340ba5127d9636fb8b62662d
Aʙɪsʜɪᴋᴛʜ Jᴏs <73195851+abishikth-jos@users.noreply.github.com>
2025-04-25 12:01:10 +0400

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,6 +3,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
-  await mongoose.connect(`${process.env.MONGODB_URI}/bookmydoctor`);
+  await mongoose.connect(`${process.env.MONGODB_URI}`);
 };
 module.exports = connectDB;
